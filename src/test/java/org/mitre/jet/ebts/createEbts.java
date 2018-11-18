98fdcf0ee5beae7015db726ce7627557009de6ad
Matthew Bartenschlag <matthew.bartenschlag@gmail.com>
2018-11-17 22:02:59 -0500

    Cleaning up...

--- src/test/java/org/mitre/jet/ebts/createEbts.java
+++ src/test/java/org/mitre/jet/ebts/createEbts.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.mitre.createEbts;
+package org.mitre.jet.ebts;
 
 import java.awt.image.BufferedImage;
 import java.io.*;
--- src/test/java/org/mitre/jet/ebts/printEbts.java
+++ src/test/java/org/mitre/jet/ebts/printEbts.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.mitre.printEbts;
+package org.mitre.jet.ebts;
 
 import java.io.IOException;
 import java.io.InputStream;
